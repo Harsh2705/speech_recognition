8f651fdd62e0602be97675a214ecb0e95008071a
Harsh Rana <61961303+Harsh2705@users.noreply.github.com>
2020-07-05 10:42:00 +0530

    Update voice.py

--- voice.py
+++ voice.py
@@ -1,5 +1,21 @@
-import pyttsx3
+import speech_recognition as sr
 
-engine=pyttsx3.init()
-engine.say("hello sir")
-engine.runAndWait()
+r=sr.Recognizer()
+
+with sr.Microphone() as source:
+	print("speak anything: ")
+	audio= r.listen(source)
+	
+	try:
+		text=r.recognize_google(audio)
+		print("you said :",format(text))
+		with open('file.txt',mode ='w') as file: 
+			file.write("Recognized text:") 
+			file.write("\n") 
+			file.write(text) 
+			print("complete")
+
+
+	except:
+		print("sorry")
+		
